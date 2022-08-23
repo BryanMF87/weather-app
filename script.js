c0f10f657118310ecf72163514daae07a9ae09de
BryanMF87 <bmfink87@gmail.com>
2022-08-23 09:29:09 -0400

    Updates

---
+++ script.js
@@ -0,0 +1,48 @@
+let weatherAPI = 'a9abcd8e70f23b882cd63a0bd29bb182';
+let searchInput = document.getElementById('search');
+const weatherDisplay = document.getElementById('weatherdisplay');
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=${weatherAPI}&units=imperial`, {mode: 'cors'})
+    .then(response => response.json())
+    .then(data => {
+        const { main, name, sys, weather } = data;
+        const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+
+        const markup = 
+            `<div id="cityname">${name}, ${sys.country}</div>
+            <div id="citytemp">${Math.round(main.temp)}<sup>°F</sup></div>
+            <img class="cityicon" src=${icon} alt=${weather[0]["main"]}>
+            <div id="conditions">${weather[0]["description"]}</div>`  
+        ;
+    weatherDisplay.innerHTML = markup;
+
+    console.log(data)
+})
+.catch(() => {
+    console.log('Could not find weather for that location. Please try again.')
+});
+
+searchInput.addEventListener('keydown', event => {
+    if (event.keyCode === 13) {
+        event.preventDefault;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${weatherAPI}&units=imperial`, {mode: 'cors'})
+            .then(response => response.json())
+            .then(data => {
+                const { main, name, sys, weather } = data;
+                const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+
+                const markup = 
+                    `<div id="cityname">${name}, ${sys.country}</div>
+                    <div id="citytemp">${Math.round(main.temp)}<sup>°F</sup></div>
+                    <img class="cityicon" src=${icon} alt=${weather[0]["main"]}>
+                    <div id="conditions">${weather[0]["description"]}</div>`
+                ;
+                weatherDisplay.innerHTML = markup;
+
+                console.log(data)
+            })
+            .catch(() => {
+                console.log('Could not find weather for that location. Please try again.')
+            });
+    };
+});   
